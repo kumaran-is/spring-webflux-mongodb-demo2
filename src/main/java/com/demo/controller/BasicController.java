4b3f503a8e2aabc190a8a9858b64e9ffcb69729f
kumaran Iyyasamy srinivasan <kumaran.x.iyyasamy.srinivasan.-nd@disney.com>
2021-10-19 13:39:12 -0400

    initial check-in

---
+++ src/main/java/com/demo/controller/BasicController.java
@@ -0,0 +1,42 @@
+package com.demo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/api/v1/basic")
+public class BasicController {
+	
+	   //  @GetMapping(value = "/flux", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+	   @GetMapping("/flux")
+	    public Flux<Integer> returnFlux(){
+
+	        return Flux.just(1,2,3,4)
+	             //   .delayElements(Duration.ofSeconds(1))
+	                .log();
+
+	    }
+
+	    @GetMapping(value = "/fluxstream", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+	    public Flux<Long> returnFluxStream(){
+
+	        return Flux.interval(Duration.ofSeconds(1))
+	                .log();
+
+	    }
+
+	    @GetMapping("/mono")
+	    public Mono<Integer> returnMono(){
+
+	        return Mono.just(1)
+	                .log();
+
+	    }
+
+}
--- src/test/java/com/demo/SpringWebfluxMongodbDemo2ApplicationTests.java
+++ src/test/java/com/demo/SpringWebfluxMongodbDemo2ApplicationTests.java
@@ -1,13 +1,100 @@
 package com.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
-@SpringBootTest
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import  static org.mockito.Mockito.when;
+
+
+import com.demo.controller.ProductController;
+import com.demo.dto.ProductDTO;
+import com.demo.service.ProductService;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest(ProductController.class)
 class SpringWebfluxMongodbDemo2ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+    private WebTestClient webTestClient;
+    @MockBean
+    private ProductService service;
+    
+    @Test
+    public void addProductTest(){
+		Mono<ProductDTO> productDTOMono=Mono.just(new ProductDTO("10","iphone 3",1,10000));
+		when(service.saveProduct(productDTOMono)).thenReturn(productDTOMono);
+
+		webTestClient.post().uri("/api/v1/products")
+				.body(Mono.just(productDTOMono),ProductDTO.class)
+				.exchange()
+				.expectStatus().isOk();//200
+
+	}
+    
+    @Test
+	public void getProductsTest(){
+		Flux<ProductDTO> productDTOFlux=Flux.just(new ProductDTO("102","mobile",1,10000),
+				new ProductDTO("103","TV",1,50000));
+		when(service.getProducts()).thenReturn(productDTOFlux);
+
+		Flux<ProductDTO> responseBody = webTestClient.get().uri("/api/v1/products")
+				.exchange()
+				.expectStatus().isOk()
+				.returnResult(ProductDTO.class)
+				.getResponseBody();
+
+		StepVerifier.create(responseBody)
+				.expectSubscription()
+				.expectNext(new ProductDTO("102","mobile",1,10000))
+				.expectNext(new ProductDTO("103","TV",1,50000))
+				.verifyComplete();
+
+	}
+    
+    @Test
+	public void getProductTest(){
+		Mono<ProductDTO> productDTOMono=Mono.just(new ProductDTO("102","mobile",1,10000));
+		when(service.getProduct(any())).thenReturn(productDTOMono);
+
+		Flux<ProductDTO> responseBody = webTestClient.get().uri("/api/v1/products/102")
+				.exchange()
+				.expectStatus().isOk()
+				.returnResult(ProductDTO.class)
+				.getResponseBody();
+
+		StepVerifier.create(responseBody)
+				.expectSubscription()
+				.expectNextMatches(p->p.getName().equals("mobile"))
+				.verifyComplete();
+	}
+    
+    @Test
+	public void updateProductTest(){
+		Mono<ProductDTO> productDTOMono=Mono.just(new ProductDTO("102","mobile",1,10000));
+		when(service.updateProduct(productDTOMono,"102")).thenReturn(productDTOMono);
+
+		webTestClient.put().uri("/api/v1/products/102")
+				.body(Mono.just(productDTOMono),ProductDTO.class)
+				.exchange()
+				.expectStatus().isOk();//200
+	}
+    
+    @Test
+	public void deleteProductTest(){
+    	given(service.deleteProduct(any())).willReturn(Mono.empty());
+		webTestClient.delete().uri("/api/v1/products/102")
+				.exchange()
+				.expectStatus().isOk();//200
 	}
 
 }
